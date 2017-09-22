bbfd992742626c7879052030766c5f89cd4e920a
DESKTOP-VMD815T\asanf <asanfilippo@xgensoftware.com>
2017-09-22 15:39:36 -0400

    .

---
+++ Codazi/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Codazi.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("About")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [Route("Contact")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [Route("Services")]
+        public ActionResult Services()
+        {
+            return View();
+        }
+    }
+}
---
+++ Codazi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Codazi.MvcApplication" Language="C#" %>
