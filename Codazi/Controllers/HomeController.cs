17654eac95a6a9f813bd78e488e255ea919f101f
DESKTOP-VMD815T\asanf <asanfilippo@xgensoftware.com>
2017-09-25 11:49:04 -0400

    Final

--- Codazi/Controllers/HomeController.cs
+++ Codazi/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
-
+using Codazi.Models;
+using com.Xgensoftware.Core;
 namespace Codazi.Controllers
 {
     public class HomeController : Controller
@@ -33,6 +36,38 @@ namespace Codazi.Controllers
         [Route("Services")]
         public ActionResult Services()
         {
+            return View();
+        }
+
+        [Route("Contact")]
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel m)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    StringBuilder s = new StringBuilder();
+                    s.Append("A new request for information has been made at codazi.com");
+                    s.Append("<br />");
+                    s.Append("<br />");
+                    s.Append(string.Format("From: {0}", m.Name));
+                    s.Append("<br />");
+                    s.Append(string.Format("Email Address: {0}", m.Email));
+                    s.Append("<br/>");
+                    s.Append(string.Format("Comment: {0}", m.Comment));
+
+                    SendMailHelper mailHelper = new SendMailHelper("smtp.gmail.com", 587, "asanfilippo@xgensoftware.com", "3620MichellE");
+                    mailHelper.SendEmail("info@xgensoftware.com", "info@Codazi.com", "Request from Codazi.com", s.ToString(), true);
+
+                    return View("Success");
+                }
+                catch (Exception ex)
+                {
+                    return PartialView("Error");
+                }
+            }
+
             return View();
         }
     }
---
+++ Codazi/Models/ContactViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Codazi.Models
+{
+    public class ContactViewModel
+    {
+ 
+
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
